acb6f505b28e84271a48f0bc6fc7e598f9583d4e
Chirag Parekh <chiragparekhn@gmail.com>
2023-08-13 14:22:25 +0530

    Added payments table and its model

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'type',
+        'details'
+    ];
+
+    public function uniqueIds()
+    {
+        return ['uuid'];
+    }
+}
