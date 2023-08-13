2355ef07ac7a155c9e7b6f16df3defc6da06e7f3
Chirag Parekh <chiragparekhn@gmail.com>
2023-08-13 15:53:34 +0530

    Added promotions table and its model

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Promotion extends Model
 {
     use HasFactory, HasUuids;
 
     protected $fillable = [
         'title',
-        'slug',
         'content',
         'metadata'
     ];
